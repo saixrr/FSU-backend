495aa48ebe2ce1c453217c0b47119cf787c4a47a
saixrr <111263091+saixrr@users.noreply.github.com>
2025-04-16 02:55:38 +0530

    env

--- app/database.py
+++ app/database.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://forfunsake04:VuMLpQZ9hI5i6ua3@fsuplayers.nzbrr8s.mongodb.net/")
+
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
 db = client.fsu_dashboard
