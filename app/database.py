9ab3f87cab92e0a8e675d18f85ca72e905415bde
saixrr <111263091+saixrr@users.noreply.github.com>
2025-04-16 13:11:00 +0530

    .env

--- app/database.py
+++ app/database.py
@@ -2,9 +2,19 @@ import motor.motor_asyncio
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+print("LOADED:", os.getenv("MONGO_URI"))  # should now show Atlas URI
+
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://forfunsake04:VuMLpQZ9hI5i6ua3@fsuplayers.nzbrr8s.mongodb.net/")
+print("Loaded MONGO_URI:", os.getenv("MONGO_URI"))
+
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
 db = client.fsu_dashboard
--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="FSU Football Player API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:8080"],
+    allow_origins = ["http://localhost:8080","https://vercel.com/saixrrs-projects/fsu-frontend"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
