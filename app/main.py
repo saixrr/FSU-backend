ab4ce217b6da53995b73777b5d888f605194ce28
saixrr <111263091+saixrr@users.noreply.github.com>
2025-04-17 01:41:42 +0530

    deploy

--- app/main.py
+++ app/main.py
@@ -6,7 +6,10 @@ app = FastAPI(title="FSU Football Player API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:8080","https://vercel.com/saixrrs-projects/fsu-frontend"],
+     allow_origins=[
+        "http://localhost:8080",
+        "https://fsu-frontend.vercel.app"  # ✅ Fix this
+    ],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
